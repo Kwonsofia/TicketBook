272091f3baf5c133fc7ee2605292ee6642950c49
HuiHyeon <n991119@gmail.com>
2020-08-20 17:21:26 +0900

    update ledgersssss

--- app/src/main/java/com/example/ledger2/LedgerActivity.java
+++ app/src/main/java/com/example/ledger2/LedgerActivity.java
@@ -19,8 +19,14 @@ import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GetTokenResult;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -50,11 +56,11 @@ public class LedgerActivity extends AppCompatActivity {
 
     String getTime;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ledger);
+
 
         textMonth = findViewById(R.id.textMonth);
         textSum = findViewById(R.id.textSum);
