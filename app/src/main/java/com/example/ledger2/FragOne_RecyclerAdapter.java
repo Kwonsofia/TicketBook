8e1a1bcd500e076c05bfbd338849ef0a02fc6ac8
WESTZERO115 <65750746+WESTZERO115@users.noreply.github.com>
2020-08-21 17:46:31 +0900

    please last
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/example/ledger2/FragOne_RecyclerAdapter.java
+++ app/src/main/java/com/example/ledger2/FragOne_RecyclerAdapter.java
@@ -1,29 +1,123 @@
 package com.example.ledger2;
 
+import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
-
-import com.google.firebase.database.core.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class FragOne_RecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+import java.util.ArrayList;
+import java.util.List;
+
+public class FragOne_RecyclerAdapter extends RecyclerView.Adapter<FragOne_RecyclerAdapter.Holder> {
+
+    private List<Schedule> list = new ArrayList<>();
+
+    public FragOne_RecyclerAdapter(List<Schedule> list) {
+        this.list = list;
+    }
+
+    // ViewHolder 생성
+    // row layout을 화면에 뿌려주고 holder에 연결
+    @Override
+    public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_adapter, parent, false);
+        Holder holder = new Holder(view);
+        return holder;
+    }
+
+
+    @Override
+    public void onBindViewHolder(Holder holder, int position) {
+        // 각 위치에 문자열 세팅
+        int itemposition = position;
+
+        holder.title.setText(list.get(itemposition).title);
+        holder.time.setText(list.get(itemposition).time+"시 ");
+        holder.date.setText("날짜: "+ list.get(itemposition).date);
+        holder.detail.setText(list.get(itemposition).date);
+    }
+
+    // 몇개의 데이터를 리스트로 뿌려줘야하는지 반드시 정의해줘야한다
+    @Override
+    public int getItemCount() {
+        return list.size(); // RecyclerView의 size return
+    }
+
+    // ViewHolder는 하나의 View를 보존하는 역할을 한다
+    public class Holder extends RecyclerView.ViewHolder{
+        TextView title;
+        TextView date;
+        TextView time;
+        TextView detail;
+
+        public Holder(View view){
+            super(view);
+
+            this.title = itemView.findViewById(R.id.adap_title);
+            this.date = itemView.findViewById(R.id.adap_date);
+            this.detail = itemView.findViewById(R.id.adap_detail);
+            this.time = itemView.findViewById(R.id.adap_time);
+        }
+    }
+
+
+
+
+    /*
+    private ArrayList<Schedule> arrayList;
+
+    public FragOne_RecyclerAdapter(ArrayList<Schedule> arrayList) {
+        this.arrayList = arrayList;
+    }
+
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public RecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_adapter, parent, false);
+        RecyclerViewHolder holder = new FragOne_RecyclerAdapter.RecyclerViewHolder(view);
 
-        // view = LayoutInflater.from(parent.getContext()).inflate(R.id.fragone_content);
-        return null;
+        return holder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull RecyclerViewHolder holder, int position) {
 
+        String title = arrayList.get(position).getTitle();
+        String detail = arrayList.get(position).getDetail();
+        String date = arrayList.get(position).getDate();
+        int hour = arrayList.get(position).getHour();
+        int min = arrayList.get(position).getMin();
+
+        holder.title.setText(title);
+        holder.time.setText(hour+"시 "+min+"분 ");
+        holder.date.setText("날짜: "+date);
+        holder.detail.setText(detail);
     }
 
     @Override
     public int getItemCount() {
         return 0;
     }
+
+    class RecyclerViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        TextView title;
+        TextView date;
+        TextView time;
+        TextView detail;
+
+        public RecyclerViewHolder(@NonNull final View itemView) {
+            super(itemView);
+
+            this.title = itemView.findViewById(R.id.adap_title);
+            this.date = itemView.findViewById(R.id.adap_date);
+            this.detail = itemView.findViewById(R.id.adap_detail);
+            this.time = itemView.findViewById(R.id.adap_time);
+
+        }
+    }*/
 }
--- app/src/main/java/com/example/ledger2/MyPage_TabOne_Musical.java
+++ app/src/main/java/com/example/ledger2/MyPage_TabOne_Musical.java
@@ -1,173 +1,142 @@
 package com.example.ledger2;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.FirebaseDatabase;
-
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.ListFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 //((MyPage) getActivity())
 public class MyPage_TabOne_Musical extends Fragment {
 
-//    private RecyclerView recyclerView_fragone;
-//    private List<Image> imageList = new ArrayList<>();
-//    private List<String> uidLists = new ArrayList<>();
+    private RecyclerView recyclerView_fragone;
 
-    //이메일 비밀번호 로그인 모듈 변수
-    private FirebaseAuth mAuth;
-    //현재 로그인 된 유저 정보를 담을 변수
-    private FirebaseUser currentUser;
-    //데이터 베이스 저장
-    private static FirebaseDatabase database;
+    private FragOne_RecyclerAdapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<Schedule> arrayList;
 
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
 
-    ArrayList<String> worksnames = new ArrayList<String>();
-    ArrayList<String> worksdetails = new ArrayList<String>();
-    CustomAdapter ca; //커스텀 어뎁터
-
-    public static MyPage_TabOne_Musical newInstance(){
-        return new MyPage_TabOne_Musical();
-    }
-
+    FirebaseAuth mFirebaseAuth;
+    FirebaseUser mFirebaseUser;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.mypage_fragone_musical, container, false);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+
+        recyclerView_fragone = (RecyclerView) rootView.findViewById(R.id.recyclerView_fragone);
+
+        arrayList = new ArrayList<>();
+        recyclerView_fragone.setHasFixedSize(true);
+        adapter = new FragOne_RecyclerAdapter(arrayList);
+        recyclerView_fragone.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView_fragone.setAdapter(adapter);
         database = FirebaseDatabase.getInstance();
-        mAuth = FirebaseAuth.getInstance();
 
-        Activity root = getActivity(); //이 클래스가 프레그먼트이기 때문에 액티비티 정보를 얻는다.
-        Log.d("myTag1", "onCreateView-1");
-        Log.d("myTag2", "onCreateView-2");
-        View view = inflater.inflate(R.layout.fragone_content,container,false);
+        databaseReference = database.getReference(mFirebaseUser.getUid()+"/Calendar"); // DB 테이블 연결
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // firebase database의 data를 받아오는 곳
+                arrayList.clear();
 
-        TextView tabone_title = (TextView) view.findViewById(R.id.fragone_title);
-        TextView tabone_details = (TextView) view.findViewById(R.id.fragone_details);
-        ListView listView = (ListView)view.findViewById(R.id.fragone_listView);
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) { // 반복문으로 List 추출
+                    Schedule item = snapshot.getValue(Schedule.class);
+                    String comp = item.getDetail();
 
-        ca = new CustomAdapter();
-        listView.setAdapter(ca);
-
-        currentUser = mAuth.getCurrentUser();
-
-
-        display();
-        return view;
-    }
-
-    //커스텀 뷰
-    class CustomAdapter extends BaseAdapter{
-        @Override
-        public int getCount() {
-            return worksnames.size();
-        }
-        @Override
-        public long getItemId(int position) {
-            return 0;
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return null;
-        }
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-
-            Activity root = getActivity(); //이 클래스가 프레그먼트이기 때문에 액티비티 정보를 얻는다.
-            Toast.makeText(root,"getView" , Toast.LENGTH_SHORT).show();
-
-            //커스텀뷰에 있는 객체들 가져오기
-            convertView = getLayoutInflater().inflate(R.layout.mypage_fragone_musical,null);
-            TextView tName = (TextView)convertView.findViewById(R.id.fragone_title);
-            TextView tDetails = (TextView)convertView.findViewById(R.id.fragone_details);
-
-
-            tName.setText(worksnames.get(position));
-            tDetails.setText(worksdetails.get(position));
-
-            return convertView;
-        }
-    }//mFirebaseUser.getUid()+"/MyPage/"
-    public void display(){
-        database.getReference(currentUser.getUid()+"/Calendar")
-                .addChildEventListener(new ChildEventListener() {
-                    @Override
-                    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                        //개수 만큼 돈다
-
-                        //Activity root = getActivity(); //이 클래스가 프레그먼트이기 때문에 액티비티 정보를 얻는다.
-                        //Toast.makeText(root,"onChildAdded-2" , Toast.LENGTH_SHORT).show();
-                        Log.d("myTag3", "onChildAdded-2");
-
-
-                        CalendarItem thankDTo = dataSnapshot.getValue(CalendarItem.class);
-                        //키값이랑 같이 넣어주기
-                        thankDTo.setdetails(dataSnapshot.getKey());
-
-                        //Activity root = getActivity(); //이 클래스가 프레그먼트이기 때문에 액티비티 정보를 얻는다.
-                        //Toast.makeText(root, thankDTo.getName()+"/"+ thankDTo.getComment(), Toast.LENGTH_SHORT).show();
-
-                        worksnames.add(thankDTo.getStitle());
-                        worksdetails.add(thankDTo.getdetails());
-
-
-                        int a = worksnames.size();
-                        String aa = String.valueOf(a);
-                        Log.d("myTagrsult", aa);
-                        //이거를 해줘야지 adapter getView가 호출이 된다!!!!중요!!!
-                        ca.notifyDataSetChanged();
-                        //Toast.makeText(root, aa , Toast.LENGTH_SHORT).show();
+                    if(comp.equals("Musical")){
+                        //  Toast.makeText(getContext()," "+comp,Toast.LENGTH_LONG).show();
+                        arrayList.add(item);
                     }
+                    adapter.notifyDataSetChanged();
+                }
+            }
 
-                    @Override
-                    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // DB를 가져오던 중 error 발생 시
+                Log.d("MyPage", String.valueOf(databaseError.toException()));
+            }
+        });
 
+        adapter = new FragOne_RecyclerAdapter(arrayList);
+        recyclerView_fragone.setAdapter(adapter);
+
+
+        Log.e("Frag", "MainFragment");
+        return rootView;
+
+
+        /*
+        View view = inflater.inflate(R.layout.mypage_fragone_musical, container, false);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+
+        recyclerView_fragone = view.findViewById(R.id.recyclerView_fragone);
+
+        recyclerView_fragone.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView_fragone.setLayoutManager(layoutManager);
+        arrayList = new ArrayList<>(); // Ledger 객체를 담을 arrayList
+
+        database = FirebaseDatabase.getInstance();
+
+        databaseReference = database.getReference(mFirebaseUser.getUid()+"/Calendar"); // DB 테이블 연결
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // firebase database의 data를 받아오는 곳
+                arrayList.clear();
+
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) { // 반복문으로 List 추출
+                    Schedule item = snapshot.getValue(Schedule.class);
+                    String comp = item.getDetail();
+
+                    if(comp.equals("Musical")){
+                        Toast.makeText(getContext()," "+comp,Toast.LENGTH_LONG).show();
+                        arrayList.add(item);
                     }
+                }
 
-                    @Override
-                    public void onChildRemoved(DataSnapshot dataSnapshot) {
+            }
 
-                    }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // DB를 가져오던 중 error 발생 시
+                Log.d("MyPage", String.valueOf(databaseError.toException()));
+            }
+        });
 
-                    @Override
-                    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+        adapter = new FragOne_RecyclerAdapter(arrayList);
+        recyclerView_fragone.setAdapter(adapter);
 
-                    }
+        adapter.notifyDataSetChanged();
 
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-
-                    }
-                });
+        return view;*/
     }
 }
-
-
-
